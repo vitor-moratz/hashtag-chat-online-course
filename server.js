ced2a20e63b4996ba8896225b74afc93d070ff10
vitor-moratz <vitor.morato80@gmail.com>
2024-02-01 23:36:45 -0300

    voltando para quando funcionava

--- server.js
+++ server.js
@@ -2,29 +2,16 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-const server = http.createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: ["https://hashtag-chat-online-course.vercel.app"],
-    methods: ["GET", "POST"]
-  }
-});
+const servidorHTTP = http.createServer(app);
+const io = require("socket.io")(servidorHTTP);
 
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  console.log("Um usuário conectou");
-
-  socket.on("nova mensagem", (msg) => {
+io.addListener("connection", (socket) => {
+  console.log("um usuário acabou de conectar");
+  socket.addListener("nova mensagem", (msg) => {
     io.emit("nova mensagem", msg);
   });
-
-  socket.on("disconnect", () => {
-    console.log("Um usuário desconectou");
-  });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor escutando na porta ${PORT}`);
-});
+servidorHTTP.listen(2000);
