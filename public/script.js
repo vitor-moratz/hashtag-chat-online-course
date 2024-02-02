903f7b8471552bf60473746f2ec77d1bc477b4a7
vitor-moratz <vitor.morato80@gmail.com>
2024-02-01 22:51:14 -0300

    agora vai 100% :]

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const botaoEnviar = document.getElementById("enviar");
 const texto = document.getElementById("texto");
 const chat = document.getElementById("mensagens");
 
-const socket = io();
+const socket = io("https://hashtag-chat-online-course.vercel.app/");
 
 botaoEnviar.addEventListener("click", () => {
   if (texto.value !== "") {
