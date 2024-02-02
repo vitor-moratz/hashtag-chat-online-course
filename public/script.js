56df2bf9e4d185c43e1e4430cf01b88be3c5e68d
vitor-moratz <vitor.morato80@gmail.com>
2024-02-01 21:15:53 -0300

    testing vercel

---
+++ public/script.js
@@ -0,0 +1,19 @@
+const botaoEnviar = document.getElementById("enviar");
+const texto = document.getElementById("texto");
+const chat = document.getElementById("mensagens");
+
+const socket = io();
+
+botaoEnviar.addEventListener("click", () => {
+  if (texto.value !== "") {
+    socket.emit("nova mensagem", texto.value);
+    texto.value = "";
+  }
+});
+
+socket.addEventListener("nova mensagem", (msg) => {
+  const novaMensagem = document.createElement("li"); 
+  novaMensagem.classList.add("mensagem");
+  novaMensagem.textContent = msg;
+  chat.appendChild(novaMensagem);
+});
